f4fe632c060967e42b2c4cc338fb6ed3de618f87
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-09-28 18:06:56 -0400

    MMAP is fully implemented and allowed support for Sym Links
    MMAP works now. Started to work towards support for Sym Links. Next step
    is to work on file and directory permissions.

--- ProblemSets/PSet2/Hunt/Hunt.c
+++ ProblemSets/PSet2/Hunt/Hunt.c
@@ -4,7 +4,7 @@
 // Jacob Koziej (EE'25) also gave me advice on using mmap 
 // Also referenced https://www.youtube.com/watch?v=m7E9piHcfr4
 #define MAXPATHNAMELENGTH 4096
-#define SIZEOFBUF 25
+#define SIZEOFBUF 4096
 
 #include <dirent.h>
 #include <stdio.h>
@@ -19,7 +19,11 @@
 /* Program Checklist
  * 1. Get File Name, store file bytes and file contents
  * 2. opendir, readdir, closedir, fprintf, strlen, chdir
- */
+ * 3. Skip dirs that don't have r-x permissions. If original path name 
+ * is not viewable, report critical error; target file must be readable 
+ * as well.
+ * 4. Seg faults when symlinked to a file in a directory with not proper perms
+ * */
 
 int fileSize;
 int numOfBuffers;
@@ -34,6 +38,7 @@ int main(int argc, char *argv[]) {
 	struct stat statBuf;
 	stat(target, &statBuf);
 	fileSize = statBuf.st_size;
+	int targetNLinks;
 	
 	targetFD = open(target, O_RDONLY, 0666);
 	searchDir(startPath);
@@ -50,7 +55,7 @@ int searchDir(char* pathName) {
 		printf("Current Dir - %s - is NULL. Print Error Message.\n", pathName);
 		exit(1);
 	}
-	struct stat statBuf;
+	struct stat statBuf, statLinkBuf;
 	char entryPath[MAXPATHNAMELENGTH];
 	int status;
 	while ((dirEntries = readdir(currentDir)) != NULL) {
@@ -63,9 +68,26 @@ int searchDir(char* pathName) {
 		strcat(entryPath, dirEntries->d_name);
 		printf("Entry Path: %s\n", entryPath);
 		if (stat(entryPath, &statBuf) == -1) {
-			printf("Could not find the item: \"%s, %s\".\n", entryPath, pathName);
-			printf("Error #: %d, String Error: %s\n", errno, strerror(errno));
-			//exit(2);
+			if (lstat(entryPath, &statLinkBuf) == 0 && S_ISLNK(statLinkBuf.st_mode)) {
+				printf("We've hit a symbolic LINK!!!!!!!!!\n");
+				if (statBuf.st_size == fileSize) {
+					printf("Same file Size!!!\n");
+					int entryFD = open(entryPath, O_RDONLY, 0666);
+					char *fileTarget = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, targetFD, 0);
+					char *fileEntry = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, entryFD, 0);
+					//printf("fileTarget:\n-----------------------------------------------------\n%s", fileTarget);
+					//printf("fileEntry:\n-----------------------------------------------------\n%s", fileEntry);
+					if (!(strcmp(fileTarget, fileEntry))) {
+						printf("SAME FILE CONTENTS WOOOOOOOOOOOO\n");
+					}
+					close(entryFD);
+				}
+				//int readLinkN = readlink(entryPath, 
+			} else {
+				printf("Could not find the item: \"%s, %s\".\n", entryPath, pathName);
+				printf("Error #: %d, String Error: %s\n", errno, strerror(errno));
+				//exit(2);
+			}
 		} else if (S_ISDIR(statBuf.st_mode)) { //Check if entry is a directory
 			strcat(entryPath, "/");
 			if (statBuf.st_mode && (S_IRUSR || S_IXUSR)) {
@@ -85,15 +107,13 @@ int searchDir(char* pathName) {
 				int entryFD = open(entryPath, O_RDONLY, 0666);
 				char *fileTarget = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, targetFD, 0);
 				char *fileEntry = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, entryFD, 0);
-				printf("fileTarget:\n-----------------------------------------------------\n%s", fileTarget);
-				printf("fileEntry:\n-----------------------------------------------------\n%s", fileEntry);
+				//printf("fileTarget:\n-----------------------------------------------------\n%s", fileTarget);
+				//printf("fileEntry:\n-----------------------------------------------------\n%s", fileEntry);
 				if (!(strcmp(fileTarget, fileEntry))) {
 					printf("SAME FILE CONTENTS WOOOOOOOOOOOO\n");
 				}
 				close(entryFD);
 			}
-		} else if (S_ISLNK(statBuf.st_mode)) {
-			printf("We've hit a symbolic LINK!!!!!!!!!\n");
 		}
 	}
 	
