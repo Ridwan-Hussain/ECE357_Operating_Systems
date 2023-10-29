b21151d8c2511a5a3011dd8fafc8d1d9fe8a3696
Ridwan Hussain <ridwanhussain2003@gmail.com>
2023-10-29 15:28:21 -0400

    More progress for launcher. Need to use dup and fds to finish launcher.

--- ProblemSets/PSet4/Pager/launcher.c
+++ ProblemSets/PSet4/Pager/launcher.c
@@ -8,8 +8,11 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int launcher(int wordgenArg);
+int waiting(char* program, int pid);
 
 int main(int argc, char* argv[]) {
 	int wordgenArg = 0;
@@ -20,62 +23,62 @@ int main(int argc, char* argv[]) {
 }
 
 int launcher(int wordgenArg) {
-	//The program will fork twice. Once normally, and the parent will fork a second time.
-	//The first child is wordgen. The 2nd child (so the child in the parent) is wordsearch.
-	//The final parent is pager. 
-	int wordgenPID=0, wordsearchPID=0;
+	//The program will fork three times. Each of the forks occur in the parent.
+	//The first child is wordgen. The 2nd child wordsearch. The third child is pager.
+	//The final parent is for the rest of the logic for the code.
+	int wordgenPID=0, wordsearchPID=0, pagerPID=0;;
 	switch (wordgenPID=fork()) {
 		case -1:
 			fprintf(stderr, "Fork failed for wordgen.\nErrno: %s, strerror: %s\n", errno, strerror(errno));
 			return(errno);
 		case 0: //wordgen
 			fprintf(stdout, "In wordgen child.\n");
-			execvp("./wordgen", "./wordgen", wordgenArg, NULL);
+			execlp("./wordgen", "./wordgen", wordgenArg, NULL);
 			break;
 		default:
-			unsigned wordgenStatus;
 			fprintf(stdout, "First parent.\n");
-			wait4(wordgenPID, wordgenStatus, 0, NULL);
-			fprintf(stdout, "Finished waiting for wordgen child.\n");
+			waiting("wordgen", wordgenPID);
 			
-			if (wordgenStatus != 0) {
-				if (WIFSIGNALED(wordgenStatus)) {
-					fprintf(stderr, "Wordgen exited with signal: %d\n", WTERMSIG(wordgenStatus));
-					return WTERMSIG(wordgenStatus);
-				} else {
-					fprintf(stderr, "Wordgen exited with return value: %d\n", WEXITSTATUS(wordgenStatus));
-					return WEXITSTATUS(wordgenStatus);
-				}
-			}
-			
-			switch (wordsearchPID = fork()) {
-				case -1:
-				fprintf(stderr, "Fork failed for wordsearch.\nErrno: %s, strerror: %s\n", errno, strerror(errno));
-				return(errno);
-			case 0: //wordsearch
-				fprintf(stdout, "In wordsearch child.\n");
-				execvp("./wordsearch", "./wordsearch", "dict.txt", NULL);
-				break;
-			default: //pager
-				unsigned wordsearchStatus;
-				fprintf(stdout, "Second parent.\n");
-				wait4(wordsearchPID, wordsearchStatus, 0, NULL);
-				fprintf(stdout, "Finished waiting for wordsearch child.\n");
-			
-				if (wordsearchStatus != 0) {
-					if (WIFSIGNALED(wordsearchStatus)) {
-						fprintf(stderr, "Wordsearch exited with signal: %d\n", WTERMSIG(wordsearchStatus));
-						return WTERMSIG(wordsearchStatus);
-					} else {
-						fprintf(stderr, "Wordsearch exited with return value: %d\n", WEXITSTATUS(wordsearchStatus));
-						return WEXITSTATUS(wordsearchStatus);
-					}
-				} 
-				
-				execvp("./pager", "./pager", NULL);
-				break;
-			}
-	}
+	switch (wordsearchPID = fork()) {
+		case -1:
+			fprintf(stderr, "Fork failed for wordsearch.\nErrno: %s, strerror: %s\n", errno, strerror(errno));
+			return(errno);
+		case 0: //wordsearch
+			fprintf(stdout, "In wordsearch child.\n");
+			execlp("./wordsearch", "./wordsearch", "dict.txt", NULL);
+			break;
+		default: 
+			fprintf(stdout, "Second parent.\n");
+			waiting("wordsearch", wordsearchPID);
 	
+	switch (pagerPID = fork()) {
+		case -1:
+			fprintf(stderr, "Fork failed for pager.\nErrno: %s, strerror: %s\n", errno, strerror(errno));
+			return(errno);
+		case 0: //pager
+			fprintf(stdout, "In pager child.\n");
+			execlp("./pager", "./pager", NULL);
+			break;
+		default: //main program logic
+			fprintf(stdout, "Final parent.\n");
+			waiting("pager", pagerPID);
+			
+			break;
+		}}} //spaced out so code isn't nested like crazy.
+	return 0;	
 }
 
+int waiting(char* program, int pid) {
+	unsigned wstatus;
+	wait4(pid, &wstatus, 0, NULL);
+	fprintf(stdout, "Finished waiting for %s child.\n", program);
+	if (wstatus != 0) {
+		if (WIFSIGNALED(wstatus)) {
+			fprintf(stderr, "%s exited with signal: %d\n", program, WTERMSIG(wstatus));
+			exit(WTERMSIG(wstatus));
+		} else {
+			fprintf(stderr, "%s exited with return value: %d\n", program, WEXITSTATUS(wstatus));
+			exit(WEXITSTATUS(wstatus));
+		}
+	}
+}
